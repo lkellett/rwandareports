57caa20e90ac1024e841e989f627e87bc603e1d3
Bailly Rurangirwa <rubailly@gmail.com>
2012-03-08 10:30:19 +0000

    updated OnInsulin.java

--- src/org/openmrs/module/rwandareports/customcalculator/OnInsulin.java
+++ src/org/openmrs/module/rwandareports/customcalculator/OnInsulin.java
@@ -7,8 +7,8 @@ import org.apache.commons.logging.LogFactory;
 import org.openmrs.DrugOrder;
 import org.openmrs.Encounter;
 import org.openmrs.EncounterType;
-import org.openmrs.Order;
 import org.openmrs.Patient;
+import org.openmrs.api.OrderService.ORDER_STATUS;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.rowperpatientreports.patientdata.definition.CustomCalculation;
@@ -34,18 +34,18 @@ public class OnInsulin implements CustomCalculation {
 
 				Patient p = result.getPatientData().getPatient();
 				List<Encounter> patientEncounters = Context.getEncounterService().getEncounters(p, null, null, null, null, diabetesEncouters, null, false);
-
-				if (patientEncounters.size() >= 1) {
-					Encounter recentEncounter = patientEncounters.get(0);  //the first encounter in the List should be the most recent one.
-					for (Order order : recentEncounter.getOrders()) {
-						if (order instanceof DrugOrder && isInsulineOrder((DrugOrder) order)) {
-
+                List<DrugOrder> patientDrugOrders = Context.getOrderService().getDrugOrdersByPatient(p, ORDER_STATUS.NOTVOIDED, false);
+                
+				if (patientEncounters.size() > 0 && patientDrugOrders.size() > 0) {
+					Encounter recentEncounter = patientEncounters.get(patientEncounters.size() - 1);  //the last encounter in the List should be the most recent one.
+					 for (DrugOrder drugOrder : patientDrugOrders) {
+						if (isInsulineOrder(drugOrder) && ! drugOrder.isDiscontinued(recentEncounter.getEncounterDatetime())) {
 							sr.setValue("Y");
-							break;
-						} else {
-							sr.setValue("N");
-						}
+							return sr;
+						} 
 					}
+					 sr.setValue("N");
+					 return sr;
 
 				} else {
 					sr.setValue("N");
@@ -65,7 +65,7 @@ public class OnInsulin implements CustomCalculation {
 		for (String string : items) {
 			try {
 				int i = Integer.parseInt(string);
-				if (i == order.getConcept().getConceptId())
+				if (i == order.getDrug().getConcept().getConceptId())
 					return true;
 			} catch (NumberFormatException e) {
 				log.error("Invalid Global property: "+GlobalPropertiesManagement.INSULIN+". Value should be a comma separated list of Integers");
